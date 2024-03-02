24db8fd4789fe5e1e5180b705ca8642927870fe5
Alessandro <sandroledesma@gmail.com>
2024-03-02 10:52:39 -0500

    add files

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Sandro is the champion';
